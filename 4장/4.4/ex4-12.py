09403c4b656ebc9b816c32e8fdfac5db6bc6c684
CMAIN\User <jiho2689@naver.com>
2023-12-07 19:59:10 +0900

    update

---
+++ 4장/4.4/ex4-12.py
@@ -0,0 +1,8 @@
+sum=0
+i=1
+
+while i <= 15000:
+    sum=sum+i
+    print("i의 값:%2d => 합계 : %d " % (i,sum))
+
+    i=i+1
---
+++ 4장/readme.md
@@ -0,0 +1,10 @@
+# 반복문
+
+### for 문법
+
+기본 사용법
+
+```py
+for i in range(n): # n번 반복
+    print("hello") # 이 내용을
+```
